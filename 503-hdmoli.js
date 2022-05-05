f8fc8e76acb3d98fdd0747481c1e706f1d21a214
tvba <104016664+tvba@users.noreply.github.com>
2022-05-05 15:03:43 +0800

    Update 503-hdmoli.js

--- 503-hdmoli.js
+++ 503-hdmoli.js
@@ -1,167 +1,466 @@
 {
-	"ua": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
-    // 首页地址 用于获取 分类和首页推荐
-	"homeUrl": "https://www.hdmoli.com",
-     "dcPlayUrl": "true",
-    // 手动设置分类，如果手动设置了分类则不使用上面的分类xpath获取分类  例如 "cateManual": {"电影": "1", "电视剧": "2"},
-	"cateManual": {"电影": "1","剧集": "2"},
-    // 首页推荐视频的节点
-	"homeVodNode": "//ul[contains(@class,'myui-vodlist clearfix')]/li/div/a",
-    // 首页推荐视频的名称
-	"homeVodName": "/@title",
-    // 二次处理正则
-    "homeVodNameR": "",
-    // 首页推荐视频的id
-	"homeVodId": "/@href",
-    // 二次处理正则
-	"homeVodIdR": "/movie/(\\w+).html",
-    // 首页推荐视频的图片
-	"homeVodImg": "/@data-original",
-    // 二次处理正则
-    "homeVodImgR": "",
-    // 首页推荐视频的简介
-	"homeVodMark": "/span[2]/text()",
-    // 二次处理正则
-    "homeVodMarkR": "",
-    // 分类页地址 {cateId} 分类id {catePg} 当前页
-	"cateUrl": "https://www.hdmoli.com/search.php?&page={catePg}&searchtype=5&order={by}&tid={cateId}&area={area}&year=&letter=&yuyan=&state=&money=&ver=&jq=",
-    // 同上面的homeVod字段 分类列表中的视频信息
-	"cateVodNode": "//ul[contains(@class,'myui-vodlist clearfix')]/li/div/a",
-	"cateVodName": "/@title",
-	"cateVodId": "/@href",
-	"cateVodIdR": "/movie/(\\w+).html",
-	"cateVodImg": "/@data-original",
-	"cateVodMark": "/span[2]/text()",
-    // 详情页地址 用于获取详情页信息 及 播放列表和地址
-	"dtUrl": "https://www.hdmoli.com/movie/{vid}.html",
-    // 详情节点
-	"dtNode": "//body",
-    // 视频名
-	"dtName": "//div[contains(@class,'myui-content__detail')]/h1/text()",
-	"dtNameR": "",
-    // 视频图片
-	"dtImg": "//div[contains(@class,'myui-content__thumb')]/a/img/@data-original",
-	"dtImgR": "",
-    // 视频分类
-	"dtCate": "//div[contains(@class,'myui-content__detail')]/p[2]/text()[2]",
-	"dtCateR": "",
-    // 视频年份
-	"dtYear": "//div[contains(@class,'myui-content__detail')]/p[2]/text()[6]",
-    // 视频简介
-	"dtDesc": "//p[contains(@class,'desc text-collapse hidden-xs')]/text()[2]",
-	"dtDescR": "",
-    // 播放源节点
-	"dtFromNode": "//h3[contains(text(), '播放地址')]",
-    // 播放源名称
-	"dtFromName": "/text()",
-    // 二次处理正则
-	"dtFromNameR": "",
-    // 播放列表节点
-	"dtUrlNode": "//ul[contains(@class,'myui-content__list sort-list clearfix')]",
-    // 播放地址节点
-	"dtUrlSubNode": "/li/a",
-    // 播放地址
-	"dtUrlId": "/@href",
-    // 二次处理正则
-	"dtUrlIdR": "/play/(\\S+).html",
-    // 剧集名称
-	"dtUrlName": "/text()",
-    // 二次处理正则
-	"dtUrlNameR": "",
-    // 播放页面的地址 {playUrl} 对应上面 dtUrlId 获取到的地址
-	"playUrl": "https://www.hdmoli.com/play/{playUrl}.html",
-    // 解析webview的user-agent
-	"playUa": "",
-    // 搜索地址
-	"searchUrl": "https://www.hdmoli.com/search.php?searchword={wd}",
-    // 同上面的homeVod字段 搜索结果中的视频信息, 这里有对苹果cms suggest搜索接口的特殊支持，参考示例中的极品影视
-	"scVodNode": "//li[contains(@class,'active  clearfix')]/div/a",
-	"scVodName": "/@title",
-	"scVodId": "/@href",
-	"scVodIdR": "/movie/(\\w+).html",
-	"scVodImg": "/@data-original",
-	"scVodMark": "/span[2]/text()",
-	"filter":{
-		"1":[
-			{
-		"key": "cateId",
+  "ua": "",
+  "homeUrl": "https://www.hdmoli.com/",
+
+  "homeVodNode": "//body/div[@class='container']//div[@class='myui-vodlist__box']/a",
+  "homeVodName": "/@title",
+  "homeVodId": "/@href",
+  "homeVodIdR": "/(.+).html",
+  "homeVodImg": "/@data-original",
+  "homeVodMark": "/span[contains(@class, 'pic-text')]/text()",
+
+  "cateManual": {
+    "电影": "1",
+    "电视剧": "2"
+  },
+
+  "cateUrl": "https://www.hdmoli.com/search.php?page={catePg}&searchtype=5&order={by}&tid={cateId}&area={area}&year={year}&letter=&yuyan=&state=&money=&ver=&jq=",
+  "cateVodNode": "//body/div[@class='container']//div[@class='myui-vodlist__box']/a",
+  "cateVodName": "/@title",
+  "cateVodId": "/@href",
+  "cateVodIdR": "/(.+).html",
+  "cateVodImg": "/@data-original",
+  "cateVodMark": "/span[contains(@class, 'pic-text')]/text()",
+
+  "dtUrl": "https://www.hdmoli.com/{vid}.html",
+  "dtNode": "//div[contains(@class,'myui-content__detail')]",
+  "dtName": "/h1/text()",
+  "dtImg": "//div[@class='thumb-div']/img/@src",
+  "dtCate": "/p/span[contains(text(), '分类')]/following-sibling::text()/text()",
+  "dtYear": "/p/span[contains(text(), '年份')]/following-sibling::text()/text()",
+  "dtArea": "/p/span[contains(text(), '地区')]/following-sibling::text()/text()",
+  "dtMark": "/p/span[contains(text(), '更新')]/following-sibling::text()/text()",
+  "dtDirector": "/p/span[contains(text(), '导演')]/following-sibling::text()/text()",
+  "dtActor": "/p/span[contains(text(), '演员')]/following-sibling::a/text()/text()",
+  "dtDesc": "//span[@class='desc text-collapse hidden-xs']/text()",
+
+  "dtFromNode": "//h3[contains(text(), '播放地址')]",
+  "dtFromName": "/text()",
+  "dtFromNameR": "",
+  "dtUrlNode": "//div[@id='playlist1']//ul",
+  "dtUrlSubNode": "/li/a",
+  "dtUrlId": "/@href",
+  "dtUrlIdR": "/(.+)",
+  "dtUrlName": "/@title",
+  "dtUrlNameR": "",
+  "playUrl": "",
+  "playUa": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36",
+
+  "searchUrl": "https://www.hdmoli.com/search.php?searchword={wd}",
+  "scVodNode": "//div[@class='container min']/ul[contains(@class, 'myui-vodlist__media')]/li",
+  "scVodName": "//a/@title",
+  "scVodId": "//a/@href",
+  "scVodIdR": "/(.+).html",
+  "scVodImg": "//a/@data-original",
+  "scVodMark": "//a/span[contains(@class, 'pic-text')]/text()",
+  "filter": {
+    "1": [
+      {
+        "key": "cateId",
         "name": "类型",
         "value": [
-          {"n": "全部","v": "1"},
-          {"n": "动作","v": "5"},
-          {"n": "爱情","v": "6"},
-          {"n": "科幻","v": "7"},
-          {"n": "恐怖","v": "8"},
-          {"n": "战争","v": "9"},
-          {"n": "喜剧","v": "10"},
-          {"n": "纪录","v": "11"},
-          {"n": "剧情","v": "12"},
-          {"n": "犯罪","v": "30"},
-          {"n": "动画","v": "32"}
-        ]},
-			{
-		"key": "area",
+          {
+            "n": "全部",
+            "v": "1"
+          },
+          {
+            "n": "动作",
+            "v": "5"
+          },
+          {
+            "n": "爱情",
+            "v": "6"
+          },
+          {
+            "n": "科幻",
+            "v": "7"
+          },
+          {
+            "n": "恐怖",
+            "v": "8"
+          },
+          {
+            "n": "战争",
+            "v": "9"
+          },
+          {
+            "n": "喜剧",
+            "v": "10"
+          },
+          {
+            "n": "纪录",
+            "v": "11"
+          },
+          {
+            "n": "剧情",
+            "v": "12"
+          },
+          {
+            "n": "犯罪",
+            "v": "30"
+          },
+          {
+            "n": "动画",
+            "v": "32"
+          }
+        ]
+      },
+      {
+        "key": "area",
         "name": "地区",
         "value": [
-          {"n": "全部","v": ""},
-          {"n": "中国","v": "中国"},
-          {"n": "美国","v": "美国"},
-          {"n": "韩国","v": "韩国"},
-          {"n": "英国","v": "英国"},
-          {"n": "日本","v": "日本"},
-          {"n": "泰国","v": "泰国"},
-          {"n": "其他","v": "其他"}
-        ]},
-
-		{
-		"key": "by",
+          {
+            "n": "全部",
+            "v": ""
+          },
+          {
+            "n": "美国",
+            "v": "美国"
+          },
+          {
+            "n": "日本",
+            "v": "日本"
+          },
+          {
+            "n": "韩国",
+            "v": "韩国"
+          },
+          {
+            "n": "英国",
+            "v": "英国"
+          },
+          {
+            "n": "泰国",
+            "v": "泰国"
+          },
+          {
+            "n": "中国",
+            "v": "中国"
+          },
+          {
+            "n": "其他",
+            "v": "其他"
+          }
+        ]
+      },
+      {
+        "key": "year",
+        "name": "年份",
+        "value": [
+          {
+            "n": "全部",
+            "v": ""
+          },
+          {
+            "n": "2022",
+            "v": "2022"
+          },
+          {
+            "n": "2021",
+            "v": "2021"
+          },
+          {
+            "n": "2020",
+            "v": "2020"
+          },
+          {
+            "n": "2019",
+            "v": "2019"
+          },
+          {
+            "n": "2018",
+            "v": "2018"
+          },
+          {
+            "n": "2017",
+            "v": "2017"
+          },
+          {
+            "n": "2016",
+            "v": "2016"
+          },
+          {
+            "n": "2015",
+            "v": "2015"
+          },
+          {
+            "n": "2014",
+            "v": "2014"
+          },
+          {
+            "n": "2013",
+            "v": "2013"
+          },
+          {
+            "n": "2012",
+            "v": "2012"
+          },
+          {
+            "n": "2011",
+            "v": "2011"
+          },
+          {
+            "n": "2010",
+            "v": "2010"
+          },
+          {
+            "n": "2009",
+            "v": "2009"
+          },
+          {
+            "n": "2008",
+            "v": "2008"
+          },
+          {
+            "n": "2007",
+            "v": "2007"
+          },
+          {
+            "n": "2006",
+            "v": "2006"
+          },
+          {
+            "n": "2005",
+            "v": "2005"
+          },
+          {
+            "n": "2004",
+            "v": "2004"
+          },
+          {
+            "n": "2003",
+            "v": "2003"
+          },
+          {
+            "n": "2002",
+            "v": "2002"
+          },
+          {
+            "n": "更早",
+            "v": "1__2001"
+          }
+        ]
+      },
+      {
+        "key": "by",
         "name": "排序",
         "value": [
-          {"n": "全部","v": "time"},
-          {"n": "时间排序","v": "time"},
-          {"n": "评分排序","v": "douban"}
-        ]}	
-		],
-
-		"2":[
-			{
-		"key": "cateId",
+          {
+            "n": "全部",
+            "v": ""
+          },
+          {
+            "n": "时间",
+            "v": "time"
+          },
+          {
+            "n": "评分",
+            "v": "douban"
+          }
+        ]
+      }
+    ],
+    "2": [
+      {
+        "key": "cateId",
         "name": "类型",
         "value": [
-          {"n": "全部","v": "2"},
-          {"n": "美剧","v": "15"},
-          {"n": "韩剧","v": "16"},
-          {"n": "日剧","v": "13"},
-          {"n": "英剧","v": "34"},
-          {"n": "港台","v": "14"},
-          {"n": "泰剧","v": "29"},
-          {"n": "动画","v": "33"},
-          {"n": "综艺","v": "39"},
-          {"n": "其他","v": "38"}
-        ]},
-			{
-		"key": "area",
+          {
+            "n": "全部",
+            "v": "2"
+          },
+          {
+            "n": "美剧",
+            "v": "15"
+          },
+          {
+            "n": "韩剧",
+            "v": "16"
+          },
+          {
+            "n": "日剧",
+            "v": "13"
+          },
+          {
+            "n": "英剧",
+            "v": "34"
+          },
+          {
+            "n": "中国",
+            "v": "14"
+          },
+          {
+            "n": "泰国",
+            "v": "29"
+          },
+          {
+            "n": "动画",
+            "v": "33"
+          },
+          {
+            "n": "综艺",
+            "v": "39"
+          },
+          {
+            "n": "其他",
+            "v": "38"
+          }
+        ]
+      },
+      {
+        "key": "area",
         "name": "地区",
         "value": [
-          {"n": "全部","v": ""},
-          {"n": "中国","v": "中国"},
-          {"n": "美国","v": "美国"},
-          {"n": "韩国","v": "韩国"},
-          {"n": "英国","v": "英国"},
-          {"n": "日本","v": "日本"},
-          {"n": "泰国","v": "泰国"},
-          {"n": "其他","v": "其他"}
-        ]},
-
-		{
-		"key": "by",
+          {
+            "n": "全部",
+            "v": ""
+          },
+          {
+            "n": "美国",
+            "v": "美国"
+          },
+          {
+            "n": "日本",
+            "v": "日本"
+          },
+          {
+            "n": "韩国",
+            "v": "韩国"
+          },
+          {
+            "n": "英国",
+            "v": "英国"
+          },
+          {
+            "n": "泰国",
+            "v": "泰国"
+          },
+          {
+            "n": "中国",
+            "v": "中国"
+          },
+          {
+            "n": "其他",
+            "v": "其他"
+          }
+        ]
+      },
+      {
+        "key": "year",
+        "name": "年份",
+        "value": [
+          {
+            "n": "全部",
+            "v": ""
+          },
+          {
+            "n": "2022",
+            "v": "2022"
+          },
+          {
+            "n": "2021",
+            "v": "2021"
+          },
+          {
+            "n": "2020",
+            "v": "2020"
+          },
+          {
+            "n": "2019",
+            "v": "2019"
+          },
+          {
+            "n": "2018",
+            "v": "2018"
+          },
+          {
+            "n": "2017",
+            "v": "2017"
+          },
+          {
+            "n": "2016",
+            "v": "2016"
+          },
+          {
+            "n": "2015",
+            "v": "2015"
+          },
+          {
+            "n": "2014",
+            "v": "2014"
+          },
+          {
+            "n": "2013",
+            "v": "2013"
+          },
+          {
+            "n": "2012",
+            "v": "2012"
+          },
+          {
+            "n": "2011",
+            "v": "2011"
+          },
+          {
+            "n": "2010",
+            "v": "2010"
+          },
+          {
+            "n": "2009",
+            "v": "2009"
+          },
+          {
+            "n": "2008",
+            "v": "2008"
+          },
+          {
+            "n": "2007",
+            "v": "2007"
+          },
+          {
+            "n": "2006",
+            "v": "2006"
+          },
+          {
+            "n": "2005",
+            "v": "2005"
+          },
+          {
+            "n": "2004",
+            "v": "2004"
+          },
+          {
+            "n": "2003",
+            "v": "2003"
+          },
+          {
+            "n": "2002",
+            "v": "2002"
+          },
+          {
+            "n": "更早",
+            "v": "1__2001"
+          }
+        ]
+      },
+      {
+        "key": "by",
         "name": "排序",
         "value": [
-          {"n": "全部","v": "time"},
-          {"n": "时间排序","v": "time"},
-          {"n": "评分排序","v": "douban"}
-        ]}	
-		]
-
-	}
+          {
+            "n": "全部",
+            "v": ""
+          },
+          {
+            "n": "时间",
+            "v": "time"
+          },
+          {
+            "n": "评分",
+            "v": "douban"
+          }
+        ]
+      }
+    ]
+  }
 }
